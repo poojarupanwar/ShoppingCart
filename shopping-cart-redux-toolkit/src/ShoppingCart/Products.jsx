d35f9019dae88336d519726f288f1f54e74aa1d5
poojarupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-21 15:15:23 +0530

    Update Products.jsx

--- shopping-cart-redux-toolkit/src/ShoppingCart/Products.jsx
+++ shopping-cart-redux-toolkit/src/ShoppingCart/Products.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      const productRes = await axios.get("https://fakestoreapi.com/products");
+      setProducts(productRes.data);
+    };
+    getProducts();
+  }, []);
   return (
     <>
-    Product
-    </>
-  )
-}
+  
+      <Container fluid>
+        <Row>
+          
+             {
+              products.map((product)=>(
+                <Col md={3}>
+                <Card style={{ paddingTop:'10px' , margin:'10px' }}>
+                  <div className="text-center">
+                  <Card.Img variant="top" src={product.image} style={{height:'200px' , width:'200px'}} />
+                  </div>
 
-export default Products
+                  <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>
+                     INR: {product.price}
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer style={{background:"white"}}>
+                  <Button variant="primary">Add To Cart</Button>
+                  </Card.Footer>
+                </Card>
+              </Col>
+              ))
+             }
+          
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Products;
