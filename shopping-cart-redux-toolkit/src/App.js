85aa647e9f7eabea1f273f374bedbc1523c190f5
poojarupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-27 00:52:14 +0530

    Update App.js

--- shopping-cart-redux-toolkit/src/App.js
+++ shopping-cart-redux-toolkit/src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
-import Products from './ShoppingCart/Products';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import Products from "./ShoppingCart/Products";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./ShoppingCart/Navbar";
+import Dashboard from "./ShoppingCart/Dashboard";
+import Cart from "./ShoppingCart/Cart";
+//import classes from react router dom library
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Navbar />}>
+      <Route index element={<Dashboard />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+    </Route>
+  )
+);
 function App() {
   return (
     <div className="App">
-       <Products/>
+      <RouterProvider router={router} />
     </div>
   );
 }
