831c83121bc101657d728abc35d720b1b563d531
poojarupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-27 00:54:17 +0530

    Create NavBarPanel.jsx

---
+++ shopping-cart-redux-toolkit/src/ShoppingCart/NavBarPanel.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./ShoppingCart.css";
+import { useNavigate } from "react-router-dom";
+const NavBarPanel = () => {
+    const navigate=useNavigate()
+  return (
+    <div className="header">
+      <img
+        className="logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+      <nav>
+        <div className="options">
+          <span onClick={()=>{navigate('/')}}>Products</span>
+        </div>
+      </nav>
+      <div className="search_bar">
+        <input type="text" />
+        <span>
+          <i></i>
+        </span>
+      </div>
+      <nav>
+        <div className="options">
+          <span className="options_upper">Hello,Pooja </span>
+          <span className="options_lower"> Account </span>
+        </div>
+        <div className="options">
+          <span className="options_upper"> Return </span>
+          <span className="options_lower"> & Orders </span>
+        </div>
+        <div className="options">
+          <span className="options_upper" onClick={()=>{navigate('/cart')}}> 0 </span>
+          <span className="options_lower" onClick={()=>{navigate('/cart')}}> My Cart </span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBarPanel;
