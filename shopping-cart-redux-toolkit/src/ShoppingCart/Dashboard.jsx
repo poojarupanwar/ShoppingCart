9803e7bfe4e7ba066233e036e1565f9ceaccd6ff
poojarupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-27 00:53:18 +0530

    Create Dashboard.jsx

---
+++ shopping-cart-redux-toolkit/src/ShoppingCart/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Products from './Products'
+
+const Dashboard = () => {
+  return (
+    <>
+    <h2>Dashboard</h2>
+    <Products/>
+    </>
+  )
+}
+
+export default Dashboard
